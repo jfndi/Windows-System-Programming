df04f1908a5ec597c8756a95e736038eb8a51513
jfndi <143571177+jfndi@users.noreply.github.com>
2023-10-20 11:52:51 +0200

    Add Booster client for PriorityBooster device driver.

---
+++ UserLand/Booster/Booster.cpp
@@ -0,0 +1,39 @@
+#include <windows.h>
+#include <stdio.h>
+
+#include "..\..\KernelLand\PriorityBooster\PriorityBoosterCommon.h"
+
+int Error(const char* message)
+{
+	printf("%s (error=%d)\n", message, GetLastError());
+
+	return EXIT_FAILURE;
+}
+
+int
+main(int argc, const char* argv[])
+{
+	if (argc < 3)
+	{
+		printf("Usage: Booster <threadid> <priority>\n");
+		return EXIT_FAILURE;
+	}
+
+	HANDLE hDevice = CreateFile(L"\\\\.\\PriorityBooster", GENERIC_WRITE,
+		FILE_SHARE_WRITE, nullptr, OPEN_EXISTING, 0, nullptr);
+	if (hDevice == INVALID_HANDLE_VALUE)
+		return Error("Failed to open device.");
+
+	ThreadData data;
+	data.ThreadId = atoi(argv[1]);
+	data.Priority = atoi(argv[2]);
+
+	DWORD returned;
+	BOOL success = DeviceIoControl(hDevice,
+		IOCTL_PRIORITY_BOOSTER_SET_PRIORITY,
+		&data, sizeof(data),
+		nullptr, 0,
+		&returned, nullptr);
+
+	CloseHandle(hDevice);
+}
