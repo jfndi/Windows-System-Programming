92a540715e3801aa7301e29a6c7debbe0a2c537d
jfndi <143571177+jfndi@users.noreply.github.com>
2023-11-06 12:09:03 +0100

    Adapt client for QUERY IOCTL.

--- UserLand/Protect/pch.h
+++ UserLand/Protect/pch.h
@@ -3,3 +3,4 @@
 #include <windows.h>
 #include <stdio.h>
 #include <vector>
+#include <algorithm>
--- UserLand/Protect/protect.cpp
+++ UserLand/Protect/protect.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+constexpr DWORD MAX_PIDS = 256;
+
 int Error(const char* msg)
 {
 	printf("%s (Error: %d).\n", msg, GetLastError());
@@ -26,6 +28,21 @@ vector<DWORD> ParsePids(const wchar_t* buffer[], int count)
 	return pids;
 }
 
+void PrintPids(vector<DWORD> pids)
+{
+	if (pids.size() == 0)
+	{
+		printf("There are no protected process.\n");
+		return;
+	}
+
+	printf("Currently running pids are:\n");
+	for (auto pid : pids)
+		printf("%d ", pid);
+
+	printf("\n");
+}
+
 int wmain(int argc, const wchar_t* argv[])
 {
 	if (argc < 2)
@@ -36,6 +53,7 @@ int wmain(int argc, const wchar_t* argv[])
 		Unknown,
 		Add,
 		Remove,
+		Query,
 		Clear
 	};
 
@@ -46,6 +64,8 @@ int wmain(int argc, const wchar_t* argv[])
 		option = Options::Remove;
 	else if (::_wcsicmp(argv[1], L"clear") == 0)
 		option = Options::Clear;
+	else if (::_wcsicmp(argv[1], L"query") == 0)
+		option = Options::Query;
 	else
 	{
 		printf("Unknown option.\n");
@@ -64,16 +84,34 @@ int wmain(int argc, const wchar_t* argv[])
 
 	if (option == Options::Add || option == Options::Remove)
 		pids = ParsePids(argv + 2, argc - 2);
+
 	switch (option)
 	{	
 	case Options::Add:
 		success = ::DeviceIoControl(hFile, IOCTL_PROCESS_PROTECT_BY_PID, pids.data(),
 			static_cast<DWORD>(pids.size()) * sizeof(DWORD), nullptr, 0, &bytes, nullptr);
 		break;
+
 	case Options::Remove:
 		success = ::DeviceIoControl(hFile, IOCTL_PROCESS_UNPROTECT_BY_PID, pids.data(),
 			static_cast<DWORD>(pids.size()) * sizeof(DWORD), nullptr, 0, &bytes, nullptr);
 		break;
+
+	case Options::Query:
+	{
+		vector<DWORD> ret_pids(MAX_PIDS, (DWORD)0);
+		success = ::DeviceIoControl(hFile, IOCTL_PROCESS_PROTECT_QUERY_PIDS, nullptr,
+			0, ret_pids.data(), static_cast<DWORD>(ret_pids.size()) * sizeof(DWORD)
+			, &bytes, nullptr);
+		if (success)
+		{
+			ret_pids.resize(count_if(ret_pids.begin(), ret_pids.end(),
+				[](DWORD pid) { return pid > 0; }));
+			PrintPids(ret_pids);
+		}
+	}
+		break;
+
 	case Options::Clear:
 		success = ::DeviceIoControl(hFile, IOCTL_PROCESS_PROTECT_CLEAR, nullptr,
 			0, nullptr, 0, &bytes, nullptr);
