8feaed51c6b8d5e7d582478d7dfe2edb2c88b01f
jfndi <143571177+jfndi@users.noreply.github.com>
2023-12-28 22:22:08 -0500

    Link problem to be fixed.

--- KernelLand/DelProtect/DelProtect.cpp
+++ KernelLand/DelProtect/DelProtect.cpp
@@ -71,7 +71,7 @@ DriverEntry(
 
 NTSTATUS
 DelProtectInstanceSetup(
-	_In_ PCFLT_RELATED_OBJECT FltObjects,
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
 	_In_ FLT_INSTANCE_SETUP_FLAGS Flags,
 	_In_ DEVICE_TYPE VolumeDeviceType,
 	_In_ FLT_FILESYSTEM_TYPE VolumeFilesystemType
@@ -386,7 +386,335 @@ DelProtectUnload(
 )
 /**
  * 
+ * Routine Description:
+ * 
+ *	This is the unload routine for this minFilter driver. This is called
+ *	when the miniFilter is about to be unloaded. We can fail this unload
+ *	request if this is not a mandatory unload indicated by the Flags
+ *	parameter.
+ * 
+ * Arguments:
+ * 
+ *	Flags - Indicating if this is mandatory unload.
+ * Return Value:
+ * 
+ *	Returns STATUS_SUCCESS.
+ * 
  */
 {
+	DBG_UNREFERENCED_PARAMETER(Flags);
 
+	PAGED_CODE();
+
+	PT_DBG_PRINT(PTDBG_TRACE_ROUTINES,
+		("DelProtect!DelProtectUnload: Entered.\n"));
+
+	return STATUS_SUCCESS;
+}
+
+/******************************************************************************
+*	MiniFilter callback routines.
+******************************************************************************/
+FLT_PREOP_CALLBACK_STATUS
+DelProtectPreOperation(
+	_Inout_ PFLT_CALLBACK_DATA Data,
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
+	_Flt_CompletionContext_Outptr_ PVOID* CompletionContext
+)
+/**
+ *
+ * Routine Descritption:
+ * 
+ *	This routine is a pre-operation dispatch routine for this miniFilter.
+ * 
+ *	This routine is non-pageable.
+ * 
+ * Arguments:
+ *	Data - Pointer to the filter callback data that is passed to us.
+ * 
+ *	FltObjects - Pointer to the FLT_RELATED_OBJECTS data structure containing
+ *		opaque handles to this filter, instance, its associtated volume and 
+ *		file object.
+ * 
+ *	CompletionContext - The context for the completion routine for this
+ *		operation.
+ * 
+ * Return Value:
+ * 
+ *	The return value is the status of the operation.
+ * 
+ */
+{
+	UNREFERENCED_PARAMETER(Data);
+	UNREFERENCED_PARAMETER(FltObjects);
+	UNREFERENCED_PARAMETER(CompletionContext);
+
+	PT_DBG_PRINT(PTDBG_TRACE_ROUTINES,
+		("DelProtect!DelProtectPreOperation: Entered.\n"));
+
+	return FLT_PREOP_SUCCESS_WITH_CALLBACK;
+}
+
+VOID
+DelProtectStatusCallback(
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
+	_In_ PFLT_IO_PARAMETER_BLOCK ParameterSnapshot,
+	_In_ NTSTATUS OperationStatus,
+	_In_ PVOID RequesterContext
+)
+/**
+ *
+ * Routine Description:
+ *
+ *	This routine is called when the given operation returns from the call
+ *	to IoCallDriver. This is useful for operations where STATUS_PENDING
+ *	means the operation was successfully queued. This is useful for OpLocks
+ *	and directory change notification operations.
+ *
+ *	This callback is called in the context of the originating thread and will
+ *	never be called at DPC level. The file object has been correctly
+ *	referenced so that you can access it. It will be automatically
+ *	dereferenced upon return.
+ *
+ *	This is a non-pageable routine.
+ *
+ * Arguments:
+ *
+ *	FltObjects - Pointer to the FLT_RELATED_OBJECTS data structure containing
+ *		opaque handles to this filter, instance, its associated volume and
+ *		file objects.
+ * 
+ *	RequesterConstex - The context for the completion routine for this
+ *		operation.
+ *
+ *	Operation Status -
+ *
+ * Return Value:
+ *
+ *	The return value is the status of the operation.
+ *
+ */
+{
+	UNREFERENCED_PARAMETER(FltObjects);
+
+	PT_DBG_PRINT(PTDBG_TRACE_ROUTINES,
+		("DelProtect!DelProtectOperationStatusCallback: Entered.\n"));
+
+	PT_DBG_PRINT(PTDBG_TRACE_OPERATION_STATUS,
+		("DelProtect!DelProtectOperationStatusCallback: Status=%08X ctx=%p IrpMj=%02X.%02X \"%s\"\n",
+			OperationStatus,
+			RequesterContext,
+			ParameterSnapshot->MajorFunction,
+			ParameterSnapshot->MinorFunction,
+			FltGetIrpName(ParameterSnapshot->MajorFunction)
+			));
+}
+
+FLT_POSTOP_CALLBACK_STATUS
+DelProtectPostOperation(
+	_Inout_ PFLT_CALLBACK_DATA Data,
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
+	_In_opt_ PVOID CompletionContext,
+	_In_ FLT_POST_OPERATION_FLAGS Flags
+)
+/**
+ * 
+ * Routine Descritpion:
+ * 
+ *	This routine is the post-operation completion routine for this
+ *	minFilter.
+ * 
+ *	This routine is non-pageable.
+ * 
+ * Arguments:
+ * 
+ *	Data - Pointer to the filter callback data that is passed to us.
+ * 
+ *	FltObjects - Pointer to the FLT_RELATED_OBJECTS data structure containing
+ *		opaque handles to this filter, instance, its associated volume and
+ *		file objects.
+ * 
+ *	CompletionContext - The completion context set in the pre-operation routine.
+ * 
+ *	Flags - Denotes whether the completion is successful or is being drained.
+ * 
+ * Return Value:
+ * 
+ *	The return value is the status of the operation.
+ * 
+ */
+{
+	UNREFERENCED_PARAMETER(Data);
+	UNREFERENCED_PARAMETER(FltObjects);
+	UNREFERENCED_PARAMETER(CompletionContext);
+	UNREFERENCED_PARAMETER(Flags);
+
+	PT_DBG_PRINT(PTDBG_TRACE_ROUTINES,
+		("DelProtect!DelProtectPostOperation: Entered\n"));
+
+	return FLT_POSTOP_FINISHED_PROCESSING;
+}
+
+FLT_PREOP_CALLBACK_STATUS
+DelProtectPreOperationNoPostOperation(
+	_Inout_ PFLT_CALLBACK_DATA Data,
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
+	_Flt_CompletionContext_Outptr_ PVOID* CompletionContext
+)
+/**
+ *
+ * Routine Description:
+ *
+ *
+ * Arguments:
+ *
+ * *	Data - Pointer to the filter callback data that is passed to us.
+ *
+ *	FltObjects - Pointerto the FLT_RELATED_OBJECTS data structure containing
+ *		opaque handles to this filter, instance, its associated volume and
+ *		file object.
+ *
+ *	CompletionContext - The context for the completion routine for this
+ *		operation.
+ *
+ * Return Value:
+ *
+ *	The return value is the status of the operation.
+ *
+ */
+{
+	UNREFERENCED_PARAMETER(Data);
+	UNREFERENCED_PARAMETER(FltObjects);
+	UNREFERENCED_PARAMETER(CompletionContext);
+
+	PT_DBG_PRINT(PTDBG_TRACE_ROUTINES,
+		("DelProtect!DelProtectPreOperationNoPostOperation: Entered\n"));
+
+	/*
+	 * This template code does not do anything with the callback data, but
+	 * rather returns FLT_PREOP_SUCCESS_NO_CALLBACK.
+	 * This passes the request down to the next miniFilter in the chain.
+	 */
+
+	return FLT_PREOP_SUCCESS_NO_CALLBACK;
+}
+
+FLT_PREOP_CALLBACK_STATUS
+DelProtectPreCreate(
+	PFLT_CALLBACK_DATA Data,
+	PCFLT_RELATED_OBJECTS FltObjects,
+	PVOID*
+)
+{
+	UNREFERENCED_PARAMETER(FltObjects);
+
+	if (Data->RequestorMode == KernelMode)
+		return FLT_PREOP_SUCCESS_NO_CALLBACK;
+
+	auto& params = Data->Iopb->Parameters.Create;
+	auto returnStatus = FLT_PREOP_SUCCESS_NO_CALLBACK;
+
+	if (params.Options & FILE_DELETE_ON_CLOSE)
+	{
+		KdPrint(("Delete on close: %wZ\n", &Data->Iopb->TargetFileObject->FileName));
+
+		if (!IsDeleteAllowed(PsGetCurrentProcess()))
+		{
+			Data->IoStatus.Status = STATUS_ACCESS_DENIED;
+			returnStatus = FLT_PREOP_COMPLETE;
+			KdPrint(("Prevent delete from IRP_MJ_CREAYE by cmd.exe\n"));
+		}
+	}
+
+	return returnStatus;
+}
+
+FLT_PREOP_CALLBACK_STATUS
+DelProtectPreSetInformation(
+	_Inout_ PFLT_CALLBACK_DATA Data,
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
+	PVOID*
+)
+{
+	UNREFERENCED_PARAMETER(Data);
+	UNREFERENCED_PARAMETER(FltObjects);
+
+	auto& params = Data->Iopb->Parameters.SetFileInformation;
+
+	if (params.FileInformationClass != FileDispositionInformation
+		&& params.FileInformationClass != FileDispositionInformationEx)
+		/*
+		 * Not a delete operation.
+		 */
+		return FLT_PREOP_SUCCESS_NO_CALLBACK;
+
+	auto info = (FILE_DISPOSITION_INFORMATION*)params.InfoBuffer;
+	if (!info->DeleteFile)
+		return FLT_PREOP_SUCCESS_NO_CALLBACK;
+
+	auto returnStatus = FLT_PREOP_SUCCESS_NO_CALLBACK;
+
+	/*
+	 * What process did this originate from? 
+	 */
+	auto process = PsGetThreadProcess(Data->Thread);
+	NT_ASSERT(process);
+
+	if (!IsDeleteAllowed(process))
+	{
+		Data->IoStatus.Status = STATUS_ACCESS_DENIED;
+		returnStatus = FLT_PREOP_COMPLETE;
+		KdPrint(("Prevent delete from IRP_MJ_SET_INFRORMATION by cmd.exe\n"));
+	}
+
+	return returnStatus;
+}
+
+bool
+IsDeleteAllowed(
+	const PEPROCESS Process
+)
+{
+	bool currentProcess = PsGetCurrentProcess() == Process;
+
+	HANDLE hProcess;
+
+	if (currentProcess)
+		hProcess = NtCurrentProcess();
+	else
+	{
+		auto status = ObOpenObjectByPointer(Process, OBJ_KERNEL_HANDLE,
+			nullptr, 0, nullptr, KernelMode, &hProcess);
+		if (!NT_SUCCESS(status))
+			return true;
+	}
+
+	auto size = 300;
+	bool allowDelete = true;
+	auto processName = (UNICODE_STRING*)ExAllocatePool2(POOL_FLAG_PAGED, size,
+		DRIVER_TAG);
+
+	if (processName)
+	{
+		RtlZeroMemory(processName, size);
+		auto status = ZwQueryInformationProcess(hProcess, ProcessImageFileName,
+			processName, size - sizeof(WCHAR), nullptr);
+
+		if (NT_SUCCESS(status))
+		{
+			KdPrint(("Delete operation from %wZ\n", processName));
+
+			if (processName->Length > 0 &&
+				wcsstr(processName->Buffer, L"\\System32\\cmd.exe") != nullptr ||
+				wcsstr(processName->Buffer, L"\\SysWOW64\\cmd.exe") != nullptr)
+				allowDelete = false;
+		}
+
+		ExFreePool(processName);
+	}
+
+	if (!currentProcess)
+		ZwClose(hProcess);
+
+	return allowDelete;
 }
